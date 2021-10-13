759b5ac4da002730fd3658f463b44fbc21d403c0
tagsg <92475986+tagsg@users.noreply.github.com>
2021-10-13 21:39:40 +0200

    Add files via upload
    اداه تشفير ادوات بايثون

---
+++ new.py
@@ -0,0 +1,7 @@
+import compileall,os,sys
+os.system("clear") 
+os.system("cd /sdcard && ls") 
+print ('pleace Make a python scarpt in file  sdcard')
+sa1=input("Enter name file Python»»»: ")
+compileall.compile_dir(sa1)
+                                 
